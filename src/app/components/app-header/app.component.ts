c1ab82559f6398e5a071339bfd6d6d886b9e27da
Marla Narazani <marla.narazani@ibm.com>
2017-06-16 23:42:15 +0200

    added navbar, changed ui

--- src/app/components/app-header/app.component.ts
+++ src/app/components/app-header/app.component.ts
@@ -1,23 +1,7 @@
 import { Component }          from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: `
-    <div class="container">
-    <div>
-    <h1>{{Logo}} {{UserInfo}}</h1>
-      <label>Search:</label>
-    <input #searchBox id="search-box" (keyup)="search(searchBox.value)" />
-    </div>
-    
-    <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/edit" routerLinkActive="active">Rating Summary</a>
-      <a routerLink="/search" routerLinkActive="active">Rate based on Number</a>
-      <a routerLink="/expand" routerLinkActive="active">Create new project</a>
-    </nav>
-    <router-outlet></router-outlet>
-    </div>
-  `,
+  templateUrl: './app-header.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/app/components/research-expand/research-expand.component.ts
+++ src/app/components/research-expand/research-expand.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Research } from '../../research';
 import { ResearchService } from '../../services/research.service';
-
+import { Location }               from '@angular/common';
 
 import { Injectable } from '@angular/core';
 import { Http }       from '@angular/http';
 import { Observable }     from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
-
 @Component({
-  selector: 'my-expansion',
+  selector: 'create-project',
   templateUrl: './research-expand.component.html',
   styleUrls: [ './research-expand.component.css' ]
 })
 export class ResearchExpandComponent implements OnInit {
   researches: Research[] = [];
-  constructor(private ResearchService: ResearchService) { }
+  constructor(
+    private researchService: ResearchService,
+    private location: Location
+    ) { }
+
   ngOnInit(): void {
-    this.ResearchService.getResearches()
+    this.researchService.getResearches()
       .then(researches => this.researches = researches);
+  }
+
+  cancel(): void {
+    this.location.back();
+  }
+
+  save(): void {
+
+    this.researchService.create(name)
+      .then(research => {
+        this.researches.push(research);
+        this.cancel();
+      });
+
   }
 }
 
