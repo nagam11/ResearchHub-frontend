649ab2a20ca4128d5ee5c49f37fecf27e7d43422
Devgen <evgenij.dauenhauer@hotmail.com>
2017-06-20 09:02:00 +0200

    project linked to server with references and some sturcture changed and added

--- src/app/components/view-project-create/view-project-create.component.ts
+++ src/app/components/view-project-create/view-project-create.component.ts
@@ -16,6 +16,9 @@ import { Injectable } from '@angular/core';
 import { Http }       from '@angular/http';
 import { Observable }     from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import {ProjectTypeService} from "../../services/projectType.service";
+import {Project} from "../../data-model/project";
+// import {Project} from "../../data-model/project";
 
 @Component({
   selector: 'create-project',
@@ -26,24 +29,42 @@ export class CreateProjectComponent implements OnInit {
   researches: Research[] = [];
   chairs: Chair[] = [];
   faculties: Faculty[] = [];
-  projectTypes: ProjectType[] = [{'_id': 0, 'projectType' : 'Master thesis' },{'_id': 1, 'projectType' : 'Bachelor thesis' },{'_id': 2, 'projectType' : 'IDP' }];
-  selectedChair: String = 'Please select';
-  selectedFaculty: String = 'Please select';
-  selectedProjectType: String = 'Please select';
-  project = {};
+  projectTypes: ProjectType[] = [];
+ // projectTypes: ProjectType[] = [{'_id': 0, 'projectType' : 'Master thesis' }, {'_id': 1, 'projectType' : 'Bachelor thesis' },{'_id': 2, 'projectType' : 'IDP' }];
+  // selectedChair: String = 'Please select';
+  selectedChair: Chair;
+ // projectTypes = {};
+  // selectedFaculty: String = 'Please select';
+  selectedFaculty: Faculty;
+  // selectedProjectType: String = 'Please select';
+  selectedProjectType: ProjectType;
+  project: Project;
+  /// /project = {};
+
+  // project: {};
 
   constructor(
     private researchService: ProjectsService,
     private chairsService: ChairsService,
     private facultiesService: FacultiesService,
+    private projectTypeService: ProjectTypeService,
     private location: Location,
     private router: Router
     ) { }
 
   ngOnInit(): void {
-    this.researchService.getProjects().then(researches => this.researches = researches);
-    this.chairsService.getChairs().then(chairs => this.chairs = chairs);
+    this.project = new Project();
+    this.selectedProjectType = new ProjectType();
+    this.selectedProjectType.protjectType = 'Select a type of project';
+    this.selectedChair = new Chair();
+    this.selectedChair.name = 'Please select';
+    this.selectedFaculty = new Faculty();
+    this.selectedFaculty.name = 'Please select';
+    // this.researchService.getProjects().then(researches => this.researches = researches);
+    this.projectTypeService.getProjectTypes().then(projectTypes => this.projectTypes = projectTypes);
     this.facultiesService.getFaculties().then(faculties => this.faculties = faculties);
+    this.chairsService.getChairs().then(chairs => this.chairs = chairs);
+
   }
 
   cancel(): void {
@@ -52,28 +73,34 @@ export class CreateProjectComponent implements OnInit {
 
   save(): void {
     // TODO show alert that title should be filled
-    //this.project.title = this.project.title.trim();
+    // this.project.title = this.project.title.trim();
    // if (!title) { return; }
     /*this.researchService.create(title)
       .then(research => {
        // this.researches.push(research);
         this.cancel();
       });*/
+    // this.project['projetType'] = this.selectedProjectType['_id'];
+    // this.project.chair = this.selectedChair._id;
+    this.project._chair = this.selectedChair;
+    this.project._projetType = this.selectedProjectType;
+    console.log(this.selectedChair);
+    // this.project['chairs'] = this.selectedChair['_id'];
     this.researchService.create(this.project);
     this.router.navigate(['/createsuccess']);
 
   }
 
   dropdownselectedProjectType(projectType: ProjectType): void {
-    this.selectedProjectType = projectType.projectType;
+    this.selectedProjectType = projectType;
   }
 
   dropdownselectedChair(chair: Chair): void {
-    this.selectedChair = chair.name;
+    this.selectedChair = chair;
   }
 
   dropdownselectedFaculty(faculty: Faculty): void {
-    this.selectedFaculty = faculty.name;
+    this.selectedFaculty = faculty;
   }
 
 }
--- src/app/data-model/project.ts
+++ src/app/data-model/project.ts
@@ -1,10 +1,20 @@
 /**
  * created by MarlaN. 18.06.2017
  */
+import {Chair} from './chair';
+import {ProjectType} from "./projectType";
 export class Project {
   _id: number;
   title: string;
+  _projetType: ProjectType;
+  _chair: Chair;
+  _language: string;
   description: string;
   advisor: string;
   supervisor: string;
+  requeredSkills: [{skill: string }];
+  ratings: string;
+  parner: string;
+  _requeredLevel: string;
+
 }
