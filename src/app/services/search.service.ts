bb3ac7d2c9d896d0640bebf8329a5ea4c34cd452
Devgen <evgenij.dauenhauer@hotmail.com>
2017-07-08 12:17:39 +0200

    project search service

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -15,6 +15,7 @@ export class SearchService extends ResearchHubService {
     let options = new RequestOptions({ headers: this.getHeaders() });
 
     // let user = this.UserService.getCurrentUser(); TODO user services
+    console.log(JSON.stringify(searchCriteria));
     return this.http.post(this.getUrlAp() + this.resource, searchCriteria, options).toPromise()
       .then(this.extractData)
       .catch(this.handleError);
